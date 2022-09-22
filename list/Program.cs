e72d7d2ed210fe9c9528660e879820eef0ec7d79
CronyAkatsuki <cronyakatsuki@tutanota.com>
2022-09-22 16:06:59 +0200

    Add project files.

---
+++ list/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] Files = Directory.GetFileSystemEntries(Directory.GetCurrentDirectory(), "*", SearchOption.TopDirectoryOnly);
+
+foreach (string file in Files)
+{
+    FileAttributes attr = File.GetAttributes(file);
+
+    FileInfo info = new FileInfo(file);
+
+    string name = info.Name;
+
+    if ((attr & FileAttributes.System) != FileAttributes.System)
+    {
+        if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(name);
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
